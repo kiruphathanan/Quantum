d8df4801d6073d00106a4b9a976d915d2790f2af
kiruphathanan <kirupha2005@gmail.com>
2026-03-21 20:50:56 +0530

    Add files via upload

---
+++ day-6/Repeat.java
@@ -0,0 +1,23 @@
+ import java.util.*;
+public class Repeat {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+        sc.close();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:arr) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:arr) {
+            if(map.get(num)==1) {
+                System.out.print(num);
+                return;
+            }
+        }
+        System.out.print("Not found");
+    }
+}
